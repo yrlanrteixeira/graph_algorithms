be735839e1495322488970460d166933ad2b44cc
Ramon Corrêa <ramon_viniciuss@outlook.com>
2024-03-23 17:42:29 -0300

    :sparkles: feat: Implementação lista de adjacência
    Foi implementada a lista de adjacência com todas as funções desejadas.
    
    NECESSÁRIO REALIZAR MAIS TESTES!!

--- graph_algorithms/src/main/java/com/graphalgorithms/application/GrafoApplication.java
+++ graph_algorithms/src/main/java/com/graphalgorithms/application/GrafoApplication.java
@@ -1,5 +1,6 @@
 package main.java.com.graphalgorithms.application;
 
+import main.java.com.graphalgorithms.implementation.GrafoListaAdjacencia;
 import main.java.com.graphalgorithms.implementation.GrafoMatrizAdjacencia;
 
 import java.util.Scanner;
@@ -13,6 +14,7 @@ public class GrafoApplication {
         System.out.println("O grafo é direcionado? (true/false): ");
         boolean isDirecionado = scanner.nextBoolean();
         GrafoMatrizAdjacencia grafo = new GrafoMatrizAdjacencia(numeroDeVertices, isDirecionado);
+        GrafoListaAdjacencia grafoLista = new GrafoListaAdjacencia(numeroDeVertices, isDirecionado);
 
         boolean continuar = true;
         while (continuar) {
@@ -22,7 +24,11 @@ public class GrafoApplication {
             System.out.println("3. Obter sucessores de um vértice");
             System.out.println("4. Obter predecessores de um vértice");
             System.out.println("5. Obter grau de um vértice");
-            System.out.println("6. Sair");
+            System.out.println("6. Teste de grafo simples");
+            System.out.println("7. Teste de grafo regular");
+            System.out.println("8. Teste de grafo completo");
+            System.out.println("9. Teste de grafo bipartido");
+            System.out.println("10. Sair");
             int opcao = scanner.nextInt();
 
             switch (opcao) {
@@ -32,34 +38,77 @@ public class GrafoApplication {
                     System.out.println("Digite o destino da aresta: ");
                     int destino = scanner.nextInt();
                     grafo.adicionarAresta(origem, destino);
+                    grafoLista.adicionarAresta(origem, destino);
                     break;
                 case 2:
                     System.out.println("Digite o vértice para obter os vizinhos: ");
                     int vertice = scanner.nextInt();
                     System.out.println("Vizinhos do vértice " + vertice + ": " + grafo.getVizinhos(vertice));
+                    System.out.println("(Lista Adjacência) Vizinhos do vértice " + vertice + ": "
+                            + grafoLista.getVizinhos(vertice));
                     break;
                 case 3:
                     System.out.println("Digite o vértice para obter os sucessores: ");
                     vertice = scanner.nextInt();
                     System.out.println("Sucessores do vértice " + vertice + ": " + grafo.getSucessores(vertice));
+                    System.out.println("(Lista Adjacência) Sucessores do vértice " + vertice + ": "
+                            + grafoLista.getSucessores(vertice));
                     break;
                 case 4:
                     System.out.println("Digite o vértice para obter os predecessores: ");
                     vertice = scanner.nextInt();
                     System.out.println("Predecessores do vértice " + vertice + ": " + grafo.getPredecessores(vertice));
+                    System.out.println("(Lista Adjacência) Predecessores do vértice " + vertice + ": "
+                            + grafo.getPredecessores(vertice));
                     break;
                 case 5:
                     System.out.println("Digite o vértice para obter o grau: ");
                     vertice = scanner.nextInt();
                     int[] graus = grafo.getGrau(vertice);
+                    int[] grausLista = grafoLista.getGrau(vertice);
                     if (graus.length == 1) {
                         System.out.println("Grau do vértice " + vertice + ": " + graus[0]);
                     } else {
                         System.out.println("Grau de entrada do vértice " + vertice + ": " + graus[0]);
                         System.out.println("Grau de saída do vértice " + vertice + ": " + graus[1]);
                     }
+
+                    System.out.println("Grau do vértice " + vertice + ": " + grausLista[0]);
                     break;
+
                 case 6:
+                    if (grafoLista.isSimples()) {
+                        System.out.println("O grafo é Simples!");
+                    } else {
+                        System.out.println("O grafo NÃO é simples!");
+                    }
+                    break;
+
+                case 7:
+                    if (grafoLista.isRegular()) {
+                        System.out.println("O grafo é Regular!");
+                    } else {
+                        System.out.println("O grafo NÃO é Regular!");
+                    }
+                    break;
+
+                case 8:
+                    if (grafoLista.isCompleto()) {
+                        System.out.println("O grafo é Completo!");
+                    } else {
+                        System.out.println("O grafo NÃO é Completo!");
+                    }
+                    break;
+
+                case 9:
+                    if (grafoLista.isBipartido()) {
+                        System.out.println("O grafo é Bipartido!");
+                    } else {
+                        System.out.println("O grafo NÃO é Bipartido!");
+                    }
+                    break;
+
+                case 10:
                     continuar = false;
                     break;
                 default:
---
+++ graph_algorithms/src/main/java/com/graphalgorithms/implementation/GrafoListaAdjacencia.java
@@ -0,0 +1,180 @@
+package main.java.com.graphalgorithms.implementation;
+
+import main.java.com.graphalgorithms.interfaces.Grafo;
+import java.util.*;
+
+public class GrafoListaAdjacencia implements Grafo {
+    private int numeroDeVertices;
+    private boolean isDirecionado;
+    private LinkedList<Integer> listaAdjacencia[];
+
+    /**
+     * Construtor do grafo de adjacência
+     * 
+     * @param numeroDeVertices Número de vértices do grafo (tipo int)
+     * @param isDirecionado    Grafo direcionado (true), grafo NÃO direcionado
+     *                         (false)
+     */
+    public GrafoListaAdjacencia(int numeroDeVertices, boolean isDirecionado) {
+        this.numeroDeVertices = numeroDeVertices;
+        this.isDirecionado = isDirecionado;
+        this.listaAdjacencia = new LinkedList[numeroDeVertices];
+
+        for (int i = 0; i < numeroDeVertices; i++) {
+            listaAdjacencia[i] = new LinkedList();
+        }
+    }
+
+    @Override
+    public boolean adicionarAresta(int origem, int destino) {
+        // Verifica se a aresta é válida
+        if (origem >= 0 && destino >= 0 && origem < numeroDeVertices && destino < numeroDeVertices) {
+            listaAdjacencia[origem].add(destino);
+
+            // Se for um grafo não direcionado, adiciona nas duas direções
+            if (!isDirecionado)
+                listaAdjacencia[destino].add(origem);
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removerAresta(int origem, int destino) {
+        // Verifica se a aresta é válida
+        if (origem < numeroDeVertices && destino < numeroDeVertices) {
+            listaAdjacencia[origem].remove(Integer.valueOf(destino));
+
+            // Se for um grafo não direcionado, adiciona nas duas direções
+            if (!isDirecionado)
+                listaAdjacencia[destino].remove(Integer.valueOf(origem));
+
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<Integer> getVizinhos(int vertice) {
+        return listaAdjacencia[vertice];
+    }
+
+    @Override
+    public List<Integer> getSucessores(int vertice) {
+        return listaAdjacencia[vertice];
+    }
+
+    @Override
+    public List<Integer> getPredecessores(int vertice) {
+        List<Integer> predecessores = new LinkedList<>();
+
+        for (int i = 0; i < vertice; i++) {
+            if (listaAdjacencia[i].contains(vertice)) {
+                predecessores.add(i);
+            }
+        }
+
+        return predecessores;
+    }
+
+    @Override
+    public int[] getGrau(int vertice) {
+        if (listaAdjacencia[vertice].size() == 0)
+            return new int[] { 0 };
+
+        if (isDirecionado) {
+            int grauEntrada = 0;
+            for (int i = 0; i < numeroDeVertices; i++) {
+                if (listaAdjacencia[i].contains(vertice))
+                    grauEntrada++;
+            }
+
+            int grauSaida = listaAdjacencia[vertice].size();
+            return new int[] { grauEntrada + grauSaida };
+        }
+
+        return new int[] { listaAdjacencia[vertice].size() };
+    }
+
+    public boolean isSimples() {
+        for (int i = 0; i < numeroDeVertices; i++) {
+            for (Integer vizinho : listaAdjacencia[i]) {
+                if (i == vizinho)
+                    return false; // Grafo possui laço, portanto não é simples
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isRegular() {
+        int[] grau = getGrau(0);
+
+        for (int i = 1; i < numeroDeVertices; i++) {
+            if (getGrau(i) != grau)
+                return false;
+        }
+
+        return true;
+    }
+
+    public boolean isCompleto() {
+        for (int i = 0; i < numeroDeVertices; i++) {
+            if (listaAdjacencia[i].size() != numeroDeVertices - 1)
+                return false;
+        }
+
+        return true;
+    }
+
+    private boolean isBipartiteUtil(int src, int[] cor) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(src);
+        cor[src] = 1;
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            for (Integer v : listaAdjacencia[u]) {
+                if (cor[v] == -1) {
+                    cor[v] = 1 - cor[u];
+                    queue.add(v);
+                } else if (cor[v] == cor[u]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isBipartido() {
+        int[] cor = new int[numeroDeVertices];
+        Arrays.fill(cor, -1);
+
+        for (int i = 0; i < numeroDeVertices; i++) {
+            if (cor[i] == -1) {
+                if (isBipartiteUtil(i, cor))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void adicionarVertice() {
+        return;
+    }
+
+    @Override
+    public int getNumeroDeVertices() {
+        return 0;
+    }
+
+    @Override
+    public boolean temAresta(int origem, int destino) {
+        return false;
+    }
+
+}
